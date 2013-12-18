963d9681ef962e28caaa2ad6fc09dd4a8a547bab
Mark Wotton <mwotton@gmail.com>
2013-12-18 18:04:14 +1100

    remove print

--- memrise.rb
+++ memrise.rb
@@ -18,5 +18,4 @@ end
 token=ENV['BMNDR_TOKEN'] or raise "must define BMNDR_TOKEN!"
 bee = Beeminder::User.new token
 items = Memrise.new("mwotton").items
-print items
 bee.send "vietnamese", Float(items.gsub(/\D/,''))
